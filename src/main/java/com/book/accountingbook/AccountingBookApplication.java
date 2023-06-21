0306dedeb7807da7ebd764699c725b4121f697be
harrionh <lip0crystall@arch.localdomain>
2023-06-21 19:35:39 +0300

    First

---
+++ src/main/java/com/book/accountingbook/AccountingBookApplication.java
@@ -0,0 +1,13 @@
+package com.book.accountingbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book/accountingbook/AccountingBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.accountingbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
